c87573b4eccc78af77a59fa9f17a139ccd8ee62c
ksw0102 <hanarudy467@gmail.com>
2023-12-05 12:35:28 +0900

    "1st Project B/E Comp!"

---
+++ deliver/src/main/java/com/tp/deliver/dto/PurchaseDto.java
@@ -0,0 +1,63 @@
+package com.tp.deliver.dto;
+
+import com.tp.deliver.model.Food;
+
+import jakarta.persistence.ManyToOne;
+
+public class PurchaseDto {
+
+	private long id;
+
+	@ManyToOne
+	private Food food;
+	
+	private int quantity;
+	
+	private String loginId;
+
+	public PurchaseDto() {}
+	public PurchaseDto(long id, Food food, int quantity, String loginId) {
+		super();
+		this.id = id;
+		this.food = food;
+		this.quantity = quantity;
+		this.loginId = loginId;
+	}
+
+	
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Food getFood() {
+		return food;
+	}
+
+	public void setFood(Food food) {
+		this.food = food;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getLoginId() {
+		return loginId;
+	}
+
+	public void setLoginId(String loginId) {
+		this.loginId = loginId;
+	}
+	
+	
+}
+
--- deliver/src/main/java/com/tp/deliver/model/Purchase.java
+++ deliver/src/main/java/com/tp/deliver/model/Purchase.java
@@ -1,11 +1,16 @@
 package com.tp.deliver.model;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 
 @Entity
 @Table(name = "dvpurchase")
@@ -20,15 +25,23 @@ public class Purchase {
 
 	private int quantity;
 
+	private String loginId;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(nullable = false, updatable = false)
+	private LocalDateTime createAt;
+	
 	public Purchase() {
 		super();
 	}
 
-	public Purchase(long id, Food food, int quantity) {
+	public Purchase(long id, Food food, int quantity, String loginId, LocalDateTime createAt) {
 		super();
 		this.id = id;
 		this.food = food;
 		this.quantity = quantity;
+		this.loginId = loginId;
+		this.createAt = createAt;
 	}
 
 	public long getId() {
@@ -53,6 +66,24 @@ public class Purchase {
 
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
-	}	
+	}
+
+	public String getLoginId() {
+		return loginId;
+	}
+
+	public void setLoginId(String loginId) {
+		this.loginId = loginId;
+	}
+
+	public LocalDateTime getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(LocalDateTime createAt) {
+		this.createAt = createAt;
+	}
+	
+	
 
 }
